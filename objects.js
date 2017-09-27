6bb9ed6e80f731fa92e87bf17375d9d26c858c60
Onesun15 <sunnysharma15@hotmail.com>
2017-09-27 11:31:12 -0400

    drill one done

---
+++ objects.js
@@ -0,0 +1,11 @@
+function createMyObject() {
+    object = {
+        foo: 'bar',
+        answerToUnivers: 42,
+        'olly olly': 'oxen free',
+        sayHello: function() {
+            return 'hello';
+        };
+    }
+    return object;
+}
