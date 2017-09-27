38c0941f6d881a41eaaf5cd94f34ad1c91bb54b3
Onesun15 <sunnysharma15@hotmail.com>
2017-09-27 14:03:27 -0400

    new drills 1 completed

--- objects.js
+++ objects.js
@@ -1,100 +1,116 @@
-//1
+// //1
 
-function createMyObject() {
-    object = {
-        foo: 'bar',
-        answerToUnivers: 42,
-        'olly olly': 'oxen free',
-        sayHello: function() {
-            return 'hello';
-        };
-    }
-    return object;
-}
+// function createMyObject() {
+//     object = {
+//         foo: 'bar',
+//         answerToUnivers: 42,
+//         'olly olly': 'oxen free',
+//         sayHello: function() {
+//             return 'hello';
+//         };
+//     }
+//     return object;
+// }
 
-//2
+// //2
 
-function updateObject(obj) {
-    obj.foo = 'foo';
-    obj.bar = 'bar';
-    obj.bizz = 'bizz';
-    obj.bang = 'bang';
+// function updateObject(obj) {
+//     obj.foo = 'foo';
+//     obj.bar = 'bar';
+//     obj.bizz = 'bizz';
+//     obj.bang = 'bang';
 
-    return obj;
-}
+//     return obj;
+// }
 
-//3
+// //3
 
-function personMaker() {
-    const person = {
-      firstName: 'Paul',
-      lastName: 'Jones',
-      fullName: function() {
-          return `${this.firstName} ${this.lastName}`;
-      }
-    };
-    return person;
-}
+// function personMaker() {
+//     const person = {
+//       firstName: 'Paul',
+//       lastName: 'Jones',
+//       fullName: function() {
+//           return `${this.firstName} ${this.lastName}`;
+//       }
+//     };
+//     return person;
+// }
 
-function keyDeleter(obj) {
-    delete obj.foo;
-    delete obj.bar;
-    return obj;
-}
+// function keyDeleter(obj) {
+//     delete obj.foo;
+//     delete obj.bar;
+//     return obj;
+// }
 
-//Second Exercise
+// //Second Exercise
 
-//1
+// //1
 
-function makeStudentsReport(data) {
-  return data.map(student => `${student.name}: ${student.grade}`);
- }
+// function makeStudentsReport(data) {
+//   return data.map(student => `${student.name}: ${student.grade}`);
+//  }
 
-//2 (First Solution)
+// //2 (First Solution)
 
-function enrollInSummerSchool(students) {
-  for (let i = 0; i < students.length; i++) {
-    for (let key in students[i]) {
-      students[i].status = 'In Summer school';
-    }
-  }
-  return students;
-}
+// function enrollInSummerSchool(students) {
+//   for (let i = 0; i < students.length; i++) {
+//     for (let key in students[i]) {
+//       students[i].status = 'In Summer school';
+//     }
+//   }
+//   return students;
+// }
 
-//2 (Second Solution)
+// //2 (Second Solution)
 
-function enrollInSummerSchool(students) {
-  students.forEach(student => console.log(student.status = 'In summer School'));
-  return students;
-}
+// function enrollInSummerSchool(students) {
+//   students.forEach(student => console.log(student.status = 'In summer School'));
+//   return students;
+// }
 
-//3
+// //3
 
-function findById(items, idNum) {
-  for (let i = 0; i < items.length; i++) {
-    if (items[i].id === idNum) {
-      return items[i];
-    }
-  }
-}
+// function findById(items, idNum) {
+//   for (let i = 0; i < items.length; i++) {
+//     if (items[i].id === idNum) {
+//       return items[i];
+//     }
+//   }
+// }
 
-//4
+// //4
 
-function validateKeys(object, expectedKeys) {
-  const keys = Object.keys(object);
+// function validateKeys(object, expectedKeys) {
+//   const keys = Object.keys(object);
 
-  if (keys.length !== expectedKeys.length) {
-    return false;
-  }
+//   if (keys.length !== expectedKeys.length) {
+//     return false;
+//   }
   
-  for (let i = 0; i < keys.length; i++) {
-    if (keys[i] === expectedKeys[i]) {
-      return true;
-    }
+//   for (let i = 0; i < keys.length; i++) {
+//     if (keys[i] === expectedKeys[i]) {
+//       return true;
+//     }
     
-    else {
-      return false;
-    }
-  }
-}
+//     else {
+//       return false;
+//     }
+//   }
+// }
 
+// const loaf = {
+//   flour: 300,
+//   water: 210
+// };
+
+// console.log(loaf.flour, loaf.water);
+
+// const loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function() {
+//     return (this.water / this.flour )*100;
+//   }
+// }
+
+// console.log(loaf.hydration());
